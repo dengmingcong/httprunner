0c65a053b4f64d93f3f9841b4cbc045194e3b1d7
Raigor Deng <raigordeng@etekcity.com.cn>
2022-03-22 15:26:56 +0800

    test: test updating dict recursively

--- examples/postman_echo/request_methods/update_json_test.py
+++ examples/postman_echo/request_methods/update_json_test.py
@@ -42,6 +42,46 @@ class TestCaseUpdateJson(HttpRunner):
             .assert_equal("body.json.bar", "$bar")
             .assert_equal("body.json.baz", "$baz")
         ),
+        Step(
+            RunRequest("test deep is True")
+            .post("/post")
+            .with_json({
+                "data": {
+                    "foo": 3,
+                    "baz": "$baz"
+                }
+            })
+            .update_json_object({
+                "data": {
+                    "foo": "$foo",
+                    "bar": "$bar"
+                }
+            })
+            .validate()
+            .assert_equal("body.json.data.foo", "$foo")
+            .assert_equal("body.json.data.bar", "$bar")
+            .assert_equal("body.json.data.baz", "$baz")
+        ),
+        Step(
+            RunRequest("test deep is False")
+            .post("/post")
+            .with_json({
+                "data": {
+                    "foo": 3,
+                    "baz": "$baz"
+                }
+            })
+            .update_json_object({
+                "data": {
+                    "foo": "$foo",
+                    "bar": "$bar"
+                }
+            }, False)
+            .validate()
+            .assert_equal("body.json.data.foo", "$foo")
+            .assert_equal("body.json.data.bar", "$bar")
+            .assert_equal("body.json.data.baz", None)
+        ),
     ]
 
 
