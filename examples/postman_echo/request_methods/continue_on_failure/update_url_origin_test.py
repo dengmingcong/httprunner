1785709b5c73a95f00ee112c76f1bdbe8c414602
Raigor Deng <raigordeng@etekcity.com.cn>
2022-07-21 18:30:09 +0800

    test: test continue_on_failure with update_url_origin

---
+++ examples/postman_echo/request_methods/continue_on_failure/update_url_origin_test.py
@@ -0,0 +1,40 @@
+import pytest
+
+from httprunner import HttpRunner, Config, Step, RunRequest
+from httprunner.exceptions import ValidationFailure
+
+
+@pytest.mark.xfail(raises=ValidationFailure)
+class TestCaseUpdateUrlOrigin(HttpRunner):
+
+    config = (
+        Config("test update url origin")
+        .variables(**{"foo": 1, "bar": 2, "baz": 3})
+        .base_url("http://bad-netloc.com")
+        .verify(False)
+        .continue_on_failure()
+    )
+
+    teststeps = [
+        Step(
+            RunRequest("update origin")
+            .post("/post")
+            .with_origin("https://postman-echo.com")
+            .with_json({"foo": "$foo", "bar": "$bar"})
+            .validate()
+            .assert_equal("body.json.foo", "$foo")
+            .assert_equal("body.json.bar", "bad")
+        ),
+        Step(
+            RunRequest("don't update origin")
+            .post("https://postman-echo.com/post")
+            .with_json({"foo": "$foo", "bar": "$bar"})
+            .validate()
+            .assert_equal("body.json.foo", "$foo")
+            .assert_equal("body.json.bar", "bad")
+        ),
+    ]
+
+
+if __name__ == "__main__":
+    TestCaseUpdateUrlOrigin().test_start()
