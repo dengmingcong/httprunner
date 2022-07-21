480c984f1b7e0d9f605d1fd8aaba9568fd42af47
Raigor Deng <raigordeng@etekcity.com.cn>
2022-07-21 18:27:58 +0800

    test: test continue_on_failure with update_form_data

---
+++ examples/postman_echo/request_methods/continue_on_failure/update_form_data_test.py
@@ -0,0 +1,41 @@
+import pytest
+
+from httprunner import HttpRunner, Config, Step, RunRequest
+from httprunner.exceptions import ValidationFailure
+
+
+@pytest.mark.xfail(raises=ValidationFailure)
+class TestCaseUpdateFormData(HttpRunner):
+
+    config = (
+        Config("test update json")
+        .variables(**{"foo": "1", "bar": "2", "baz": "3"})
+        .base_url("https://postman-echo.com")
+        .verify(False)
+        .continue_on_failure()
+    )
+
+    teststeps = [
+        Step(
+            RunRequest("with_data has not been called")
+            .post("/post")
+            .update_form_data({"foo": "$foo", "bar": "$bar"})
+            .validate()
+            .assert_equal("body.form.foo", "$foo")
+            .assert_equal("body.form.bar", "bad")
+        ),
+        Step(
+            RunRequest("with_data has been called")
+            .post("/post")
+            .with_data({"foo": "3", "baz": "$baz"})
+            .update_form_data({"foo": "$foo", "bar": "$bar"})
+            .validate()
+            .assert_equal("body.form.foo", "$foo")
+            .assert_equal("body.form.bar", "$bar")
+            .assert_equal("body.form.baz", "bad")
+        ),
+    ]
+
+
+if __name__ == "__main__":
+    TestCaseUpdateFormData().test_start()
