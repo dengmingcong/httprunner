73ec22bbf7463211e17b5fc87f905d2b077b2480
Raigor Deng <raigordeng@etekcity.com.cn>
2022-10-26 20:42:35 +0800

    feat: add method clear() to clear validators and extract

--- httprunner/testcase.py
+++ httprunner/testcase.py
@@ -79,6 +79,10 @@ class StepRequestValidation(object):
     def __init__(self, step_context: TStep):
         self._step_context = step_context
 
+    def clear(self):
+        """Clear all validators added."""
+        self._step_context.validators.clear()
+
     def assert_equal(
         self, jmes_path: Text, expected_value: Any, message: Text = ""
     ) -> "StepRequestValidation":
@@ -351,6 +355,10 @@ class StepRequestExport(object):
 class StepRequestExtraction(object):
     def __init__(self, step_context: TStep):
         self._step_context = step_context
+
+    def clear(self):
+        """Clear extractors already added."""
+        self._step_context.extract = {}
 
     def with_jmespath(self, jmes_path: Text, var_name: Text) -> "StepRequestExtraction":
         self._step_context.extract[var_name] = jmes_path
