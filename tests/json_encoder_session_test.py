e5003eb97935e650068eea12872096d5ada5bd1f
dengmingcong <mc.deng@outlook.com>
2022-12-23 16:08:38 +0800

    test: BytesEncoder

--- tests/json_encoder_session_test.py
+++ tests/json_encoder_session_test.py
@@ -1,3 +1,6 @@
+import json
+
+from httprunner.json_encoders import BytesEncoder
 from httprunner.models import StepData
 from httprunner.client import HttpSession
 
@@ -9,3 +12,11 @@ def test_step_data():
         }
     )
     print(step_data.json())
+
+
+def test_bytes_encoder():
+    export_vars = {
+        "foo": "foo",
+        "bar": b"bar"
+    }
+    print(json.dumps(export_vars, indent=4, cls=BytesEncoder))
