5d4a2c3e5c59a935ea50797125b24061debffbb7
Raigor Deng <raigordeng@etekcity.com.cn>
2023-02-01 10:16:04 +0800

    fix: class instance is not JSON serializable

--- httprunner/json_encoders.py
+++ httprunner/json_encoders.py
@@ -6,7 +6,13 @@ class ExportVariableEncoder(json.JSONEncoder):
     JSON encoder for export vars.
     """
 
-    def default(self, obj):
-        if isinstance(obj, bytes):
-            return repr(obj)
-        return json.JSONEncoder.default(self, obj)
+    def default(self, o):
+        # bytes
+        if isinstance(o, bytes):
+            return repr(o)
+
+        # has attribute __dict__
+        if hasattr(o, "__dict__"):
+            return o.__dict__
+
+        return json.JSONEncoder.default(self, o)
