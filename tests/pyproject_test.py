2aa9ce31c2f05545dc910a6c96ea4d7ac9cda019
dengmingcong <mc.deng@outlook.com>
2023-08-15 11:22:24 +0800

    test: add validators for PyProjectTomlKey

---
+++ tests/pyproject_test.py
@@ -0,0 +1,35 @@
+from typing import Any
+
+import pytest
+
+from httprunner.pyproject import PyProjectTomlKey, pyproject_toml_data
+
+
+class TestPyProjectToml:
+    def test_additional_validator(self):
+        def name_must_be_str(v: Any):
+            if not isinstance(v, str):
+                raise TypeError
+
+        # expect to fail
+        def name_must_be_int(v: Any):
+            if not isinstance(v, int):
+                raise TypeError
+
+        class CorrectProjectMeta:
+            name = PyProjectTomlKey(
+                pyproject_toml_data, "tool.poetry.name", True, name_must_be_str
+            )
+
+        class IncorrectProjectMeta:
+            name = PyProjectTomlKey(
+                pyproject_toml_data,
+                "tool.poetry.name",
+                True,
+                name_must_be_str,
+                name_must_be_int,
+            )
+
+        assert CorrectProjectMeta().name == "httprunner"
+        with pytest.raises(TypeError):
+            assert IncorrectProjectMeta().name == "httprunner"
