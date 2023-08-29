8b1415de0248246f20a82c1ce6f745e0c5bd2967
dengmingcong <mc.deng@outlook.com>
2023-08-29 17:28:28 +0800

    fix: functions imported into debugtalk.py will not be recognized as debugtalk functions in some conditions
    exception: `FunctionNotFound` error will be raised
    condition: referenced HttpRunner subclasses found in dependencies

--- httprunner/pytestplugin.py
+++ httprunner/pytestplugin.py
@@ -1,6 +1,9 @@
 import pytest
+from _pytest.config.findpaths import get_common_ancestor, get_dirs_from_args
+from _pytest.pathlib import absolutepath
 
 from httprunner import Config, HttpRunner
+from httprunner.loader import load_project_meta
 
 
 def pytest_addoption(parser):
@@ -11,6 +14,11 @@ def pytest_addoption(parser):
         "--continue-on-failure",
         action="store_true",
         help="next steps will continue running if one step did not pass validation",
+    )
+    parser.addoption(
+        "--debugtalk-py-file",
+        action="store",
+        help="load debugtalk functions from `FILE` instead of trying to locate one debugtalk.py file",
     )
 
 
@@ -43,3 +51,22 @@ def clean_session_variables(request, is_httprunner_test):
         return
 
     request.instance.with_variables({})
+
+
+def pytest_sessionstart(session):
+    """
+    Called after the Session object has been created and before performing collection and entering the run test loop.
+    """
+    # load debugtalk functions from `FILE` specified by option instead of trying to locate one debugtalk.py file
+    if debugtalk_py_file := session.config.getoption("--debugtalk-py-file"):
+        debugtalk_py_file = absolutepath(debugtalk_py_file)
+
+        if not debugtalk_py_file.is_file():
+            raise FileNotFoundError(f"debugtalk.py file not found: {debugtalk_py_file}")
+
+        load_project_meta(debugtalk_py_file.as_posix())
+    else:
+        # locate common ancestor directory of all test files and search recursively upwards to find debugtalk.py file
+        dirs = get_dirs_from_args(session.config.args)
+        common_ancestor = get_common_ancestor(dirs)
+        load_project_meta(common_ancestor.as_posix())
