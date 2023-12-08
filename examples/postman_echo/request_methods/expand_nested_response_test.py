cec6ae321462195378e65e4746040ccef5a82e12
dengmingcong <mc.deng@outlook.com>
2023-12-08 17:11:50 +0800

    refactor: denote case sensitivity

--- examples/postman_echo/request_methods/expand_nested_response_test.py
+++ examples/postman_echo/request_methods/expand_nested_response_test.py
@@ -10,92 +10,87 @@ class TestExpandResponseNestedJson(HttpRunner):
 
     teststeps = [
         Step(
-            RunRequest("request with header X-Json-Control: expand, body contain Chinese chars")
+            RunRequest(
+                "request with header X-Json-Control: expand, body contain Chinese chars"
+            )
             .post("/post")
             .with_headers(**{"X-Json-Control": "expand"})
-            .with_json({
-                "int": 100,
-                "float": 100.02,
-                "str": "some string",
-                "strWithChinese": "我是中文",
-                "dict": {
-                    "foo": "foo"
-                },
-                "dictWithList": {
-                    "bar": [
-                        {
-                            "nest": "{\"a\":\"中文\",\"b\":\"b\"}",
-                        }
-                    ]
-                }
-            })
-            .validate()
-            .assert_equal("status_code", 200)
-            .assert_equal(
-                "body.json.dictWithList.bar[0].nest",
+            .with_json(
                 {
-                    "a": "中文",
-                    "b": "b"
+                    "int": 100,
+                    "float": 100.02,
+                    "str": "some string",
+                    "strWithChinese": "我是中文",
+                    "dict": {"foo": "foo"},
+                    "dictWithList": {
+                        "bar": [
+                            {
+                                "nest": '{"a":"中文","b":"b"}',
+                            }
+                        ]
+                    },
                 }
             )
+            .validate()
+            .assert_equal("status_code", 200)
+            .assert_equal("body.json.dictWithList.bar[0].nest", {"a": "中文", "b": "b"})
         ),
         Step(
             RunRequest("request with header X-Json-Control: not-expand")
             .post("/post")
             .with_headers(**{"X-Json-Control": "not-expand"})
-            .with_json({
-                "dictWithList": {
-                    "bar": [
-                        {
-                            "nest": "{\"a\":\"中文\",\"b\":\"b\"}",
-                        }
-                    ]
+            .with_json(
+                {
+                    "dictWithList": {
+                        "bar": [
+                            {
+                                "nest": '{"a":"中文","b":"b"}',
+                            }
+                        ]
+                    }
                 }
-            })
+            )
             .validate()
             .assert_equal("status_code", 200)
-            .assert_equal(
-                "body.json.dictWithList.bar[0].nest",
-                "{\"a\":\"中文\",\"b\":\"b\"}"
-            )
+            .assert_equal("body.json.dictWithList.bar[0].nest", '{"a":"中文","b":"b"}')
         ),
         Step(
-            RunRequest("request with header x-json-control: expand (all lowercase)")
+            RunRequest(
+                "request with header `x-json-control: expand` (case sensitive, all lowercase will not take effect)"
+            )
             .post("/post")
             .with_headers(**{"x-json-control": "expand"})
-            .with_json({
-                "dictWithList": {
-                    "bar": [
-                        {
-                            "nest": "{\"a\":\"中文\",\"b\":\"b\"}",
-                        }
-                    ]
+            .with_json(
+                {
+                    "dictWithList": {
+                        "bar": [
+                            {
+                                "nest": '{"a":"中文","b":"b"}',
+                            }
+                        ]
+                    }
                 }
-            })
+            )
             .validate()
             .assert_equal("status_code", 200)
-            .assert_equal(
-                "body.json.dictWithList.bar[0].nest",
-                "{\"a\":\"中文\",\"b\":\"b\"}"
-            )
+            .assert_equal("body.json.dictWithList.bar[0].nest", '{"a":"中文","b":"b"}')
         ),
         Step(
             RunRequest("request without header X-Json-Control")
             .post("/post")
-            .with_json({
-                "dictWithList": {
-                    "bar": [
-                        {
-                            "nest": "{\"a\":\"中文\",\"b\":\"b\"}",
-                        }
-                    ]
+            .with_json(
+                {
+                    "dictWithList": {
+                        "bar": [
+                            {
+                                "nest": '{"a":"中文","b":"b"}',
+                            }
+                        ]
+                    }
                 }
-            })
+            )
             .validate()
             .assert_equal("status_code", 200)
-            .assert_equal(
-                "body.json.dictWithList.bar[0].nest",
-                "{\"a\":\"中文\",\"b\":\"b\"}"
-            )
+            .assert_equal("body.json.dictWithList.bar[0].nest", '{"a":"中文","b":"b"}')
         ),
     ]
