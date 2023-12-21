30189efd185388a69a3720c1b0a99c402d2fa52e
dengmingcong <mc.deng@outlook.com>
2023-12-21 21:17:57 +0800

    test: retry step

--- examples/postman_echo/request_methods/request_with_retry_test.py
+++ examples/postman_echo/request_methods/request_with_retry_test.py
@@ -15,33 +15,29 @@ class TestCaseRequestWithRetry(HttpRunner):
                 "foo2": "config_bar2",
                 "expect_foo1": "config_bar1",
                 "expect_foo2": "config_bar2",
-                "sum_v": 0
+                "array": [],
             }
         )
         .base_url("https://postman-echo.com")
         .verify(False)
         .export(*["foo3"])
-        .locust_weight(2)
     )
 
     teststeps = [
         Step(
             RunRequest("get with params and retry 3 times")
             .retry_on_failure(3, 0.5)
-            .with_variables(
-                **{"foo1": "bar11", "foo2": "bar21"}
-            )
-            .get("/get")
-            .with_params(**{"foo1": "$foo1", "foo2": "$foo2", "sum_v": "${sum_two($sum_v, 1)}"})
-            .with_headers(**{"User-Agent": "HttpRunner/${get_httprunner_version()}"})
+            .with_variables(**{"foo1": "bar11", "foo2": "bar21"})
+            .post("/post")
+            .with_json({"foo1": "$foo1", "foo2": "$foo2", "array": "$array"})
+            .teardown_hook("${pyexp(array.append(1))}")
             .extract()
-            .with_jmespath("body.args.foo2", "foo3")
-            .with_jmespath("body.args.sum_v", "sum_v")
+            .with_jmespath("body.json.foo2", "foo3")
             .validate()
             .assert_equal("status_code", 200)
-            .assert_equal("body.args.foo1", "bar11")
-            .assert_equal("body.args.sum_v", "4")
-            .assert_equal("body.args.foo2", "bar21")
+            .assert_equal("body.json.foo1", "bar11")
+            .assert_equal("body.json.foo2", "bar21")
+            .assert_equal("body.json.array", [1, 1, 1])
         )
     ]
 
