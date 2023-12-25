df01360887d11901dd163db27a60e149c6dcd871
dengmingcong <mc.deng@outlook.com>
2023-12-25 17:52:17 +0800

    perf: print variable names only when VariableNotFound raised

--- httprunner/parser.py
+++ httprunner/parser.py
@@ -274,7 +274,8 @@ def get_mapping_variable(
         return variables_mapping[variable_name]
     except KeyError:
         raise exceptions.VariableNotFound(
-            f"{variable_name} not found in {variables_mapping}", variable_name
+            f"`{variable_name}` not found, available vars: {variables_mapping.keys()}",
+            variable_name,
         )
 
 
@@ -365,7 +366,8 @@ def parse_string(
                 # get the name not defined from exception, e.g. name 'baz' is not defined
                 name_not_found = str(ne).split("'")[1]
                 raise VariableNotFound(
-                    f"{name_not_found} not found in {variables_mapping}", name_not_found
+                    f"`{name_not_found}` not found, available vars: {variables_mapping.keys()}",
+                    name_not_found,
                 ) from ne
 
     try:
@@ -407,7 +409,7 @@ def parse_string(
                 expression_eval_value = eval(raw_expression, variables_mapping_copy)
             except NameError as ex:
                 raise exceptions.VariableNotFound(
-                    f"{ex}, all variables: {variables_mapping}"
+                    f"{ex}, available vars: {variables_mapping.keys()}"
                 )
             except Exception as ex:
                 raise ValueError(
