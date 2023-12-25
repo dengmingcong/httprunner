ab15a2ddd8b296c5b4688687633e1076db5cc424
dengmingcong <mc.deng@outlook.com>
2023-12-25 17:46:02 +0800

    test: fixture will fail fast

---
+++ examples/postman_echo/request_methods/continue_on_failure/fixture_fail_fast_test.py
@@ -0,0 +1,45 @@
+import pytest
+
+from httprunner import HttpRunner, Config, Step, RunRequest
+from httprunner.exceptions import ValidationFailure
+
+
+class BasePost(HttpRunner):
+
+    config = Config("base post").base_url("https://postman-echo.com").verify(False)
+
+    teststeps = [
+        Step(
+            RunRequest("")
+            .post("/post")
+            .with_data("foo-bar")
+            .validate()
+            .assert_equal("status_code", 300)
+        )
+    ]
+
+
+@pytest.fixture(autouse=True)
+def bad_post():
+    (BasePost().run())
+
+
+@pytest.mark.xfail(raises=ValidationFailure)
+class TestFixtureFailFast(HttpRunner):
+
+    config = (
+        Config("fixture will fail fast")
+        .continue_on_failure()
+        .base_url("https://postman-echo.com")
+        .verify(False)
+    )
+
+    teststeps = [
+        Step(
+            RunRequest("")
+            .post("/post")
+            .with_data("foo-bar")
+            .validate()
+            .assert_equal("status_code", 200)
+        )
+    ]
