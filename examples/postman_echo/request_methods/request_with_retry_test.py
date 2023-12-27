57741b8b8ecec85d709b2f2b7315aa8ac558383b
dengmingcong <mc.deng@outlook.com>
2023-12-27 19:32:17 +0800

    test: failed steps will not export vars

--- examples/postman_echo/request_methods/request_with_retry_test.py
+++ examples/postman_echo/request_methods/request_with_retry_test.py
@@ -2,7 +2,10 @@
 # FROM: request_methods/request_with_functions.yml
 import time
 
+import pytest
+
 from httprunner import HttpRunner, Config, Step, RunRequest
+from httprunner.exceptions import ValidationFailure
 from httprunner.testcase import HttpRunnerRequest, RequestConfig
 
 
@@ -72,5 +75,26 @@ class TestCaseRequestWithRetry(HttpRunner):
     ]
 
 
-if __name__ == "__main__":
-    TestCaseRequestWithRetry().test_start()
+@pytest.mark.xfail(raises=ValidationFailure)
+class TestFailStepWillNotExportVars(HttpRunner):
+    config = (
+        Config("test fail step will not export vars")
+        .variables(**{"sum_v": 0})
+        .base_url("https://postman-echo.com")
+        .verify(False)
+    )
+
+    teststeps = [
+        Step(
+            RunRequest("fail step will not export vars")
+            .retry_on_failure(3, 0.5)
+            .get("/get")
+            .with_params(**{"sum_v": "${sum_two($sum_v, 1)}"})
+            .with_headers(**{"User-Agent": "HttpRunner/${get_httprunner_version()}"})
+            .extract()
+            .with_jmespath("body.args.sum_v", "sum_v")
+            .validate()
+            .assert_equal("status_code", 200)
+            .assert_equal("body.args.sum_v", "4")
+        )
+    ]
