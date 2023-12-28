6926be684d4f95d33389ee4b64ee106aafcd19a9
dengmingcong <mc.deng@outlook.com>
2023-12-28 16:13:27 +0800

    refactor: convert parsed args to int/float

--- httprunner/core/runner/retry.py
+++ httprunner/core/runner/retry.py
@@ -20,24 +20,15 @@ def parse_retry_args(
             step.max_retry_times, step_context_variables, functions
         )
 
-        if not isinstance(parsed_max_retry_times, int):
-            raise ValueError(
-                f"max_retry_times should be int after parsing, but got: {type(parsed_max_retry_times)}"
-            )
-        step.max_retry_times = parsed_max_retry_times
-        step.remaining_retry_times = parsed_max_retry_times
+        step.max_retry_times = int(parsed_max_retry_times)
+        step.remaining_retry_times = int(parsed_max_retry_times)
 
     if isinstance(step.retry_interval, str):
         parsed_retry_interval = parse_data(
             step.retry_interval, step_context_variables, functions
         )
 
-        if not isinstance(parsed_retry_interval, (int, float)):
-            raise ValueError(
-                f"retry_interval should be int or float after parsing, but got: {type(parsed_retry_interval)}"
-            )
-
-        step.retry_interval = parsed_retry_interval
+        step.retry_interval = float(parsed_retry_interval)
 
     step.is_retry_args_resolved = True
 
