7a5289bd76c9f15252ab7430de1e49d003f04f99
VESYNC\linkhao <link.hao@vesync.com>
2024-03-08 16:19:42 +0800

    fix: mock_body 添加类型声明

--- httprunner/core/testcase/step/mock/mock.py
+++ httprunner/core/testcase/step/mock/mock.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from httprunner.models import TStep
 
 
@@ -6,6 +8,6 @@ class MockMixin:
 
     _step_context: TStep
 
-    def mock(self, mock_body=None):
+    def mock(self, mock_body: Union[dict, str]):
         self._step_context.mock_body = mock_body
         return self
