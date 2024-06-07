f62928fd9bb97653a3aef00e790989b7868aebad
dengmingcong <mc.deng@outlook.com>
2024-06-07 09:06:29 +0800

    feat: add main entrance function assert_equals

---
+++ httprunner/builtin/jsoncomparator/json_assert.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from httprunner.builtin.jsoncomparator.comparator import JSONComparator
+
+
+def assert_equals(
+    expected: Union[dict, list],
+    actual: Union[dict, list],
+    is_strict: bool,
+    message: str,
+) -> None:
+    """Assert that the JSON provided matches the expected JSON.
+
+    :param expected: the expected JSON.
+    :param actual: the JSON to compare against the expected JSON.
+    :param is_strict: compare the JSON strictly or not.
+    :param message: the message to display if the assertion fails.
+    """
+    json_comparator = JSONComparator(is_strict)
+    result = json_comparator.compare_json(expected, actual)
+
+    if not result.is_success:
+        raise AssertionError(f"{message}\n{result.fail_messages}")
