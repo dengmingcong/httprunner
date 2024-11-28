faa24fc32ab0edd9795ba7006cf1c6ab7c979201
dengmingcong <mc.deng@outlook.com>
2024-11-28 16:01:13 +0800

    test: nested dict

--- tests/builtin/jsoncomparator/comparator/strict_mode_test.py
+++ tests/builtin/jsoncomparator/comparator/strict_mode_test.py
@@ -27,3 +27,39 @@ class TestJSONComparatorStrictMode:
         result = self.json_comparator.compare_json({"a": [1, 2, 3]}, {"a": [3, 2, 1]})
         print(result.fail_messages)
         assert not result.is_success
+
+    def test_nested(self):
+        expected = {
+            "id": 1,
+            "address": {
+                "addr1": "123 Main",
+                "addr2": None,
+                "city": "Houston",
+                "state": "TX",
+            },
+        }
+        actual_pass = {
+            "id": 1,
+            "address": {
+                "addr1": "123 Main",
+                "addr2": None,
+                "city": "Houston",
+                "state": "TX",
+            },
+        }
+        actual_fail = {
+            "id": 1,
+            "address": {
+                "addr1": "123 Main",
+                "addr2": None,
+                "city": "Austin",
+                "state": "TX",
+            },
+        }
+
+        result = self.json_comparator.compare_json(expected, actual_pass)
+        assert result.is_success
+
+        result = self.json_comparator.compare_json(expected, actual_fail)
+        print(result.fail_messages)
+        assert not result.is_success
