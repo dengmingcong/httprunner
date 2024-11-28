5884e53252ba8557985bbcf7e8072729529ae8dc
dengmingcong <mc.deng@outlook.com>
2024-11-28 17:48:15 +0800

    test: json_contains_with_java

--- examples/postman_echo/validator/validator_json_assert_with_java_test.py
+++ examples/postman_echo/validator/validator_json_assert_with_java_test.py
@@ -4,9 +4,9 @@ from httprunner import Config, HttpRunner, RunRequest, Step
 from httprunner.exceptions import ValidationFailure
 
 
-class TestValidatorJsonAssert(HttpRunner):
+class TestValidatorJsonAssertWithJava(HttpRunner):
     config = (
-        Config("validate with json assert")
+        Config("validate with json assert with java")
         .base_url("https://postman-echo.com")
         .verify(False)
     )
@@ -18,37 +18,17 @@ class TestValidatorJsonAssert(HttpRunner):
             .with_json({"foo": "foo", "bar": [1, 2, 3, 4], "baz": 1})
             .validate()
             .assert_equal("status_code", 200)
-            .assert_json_contains(
-                "body.json",
-                (
-                    {"foo": "foo", "bar": [4, 3, 2, 1], "baz": 1.0},
-                    {"ignore_numeric_type_changes": True},
-                ),
-            )
-            .assert_json_equal(
-                "body.json",
-                (
-                    {
-                        "foo": "foo",
-                        "bar": [1, 2, 3, 4],
-                        "baz": 1.0,
-                    },
-                    {"ignore_numeric_type_changes": True},
-                ),
-            )
-            .assert_json_contains(
+            .assert_json_contains_with_java(
                 "body.json",
                 {"foo": "foo", "bar": [4, 3, 2, 1], "baz": 1.0},
-                ignore_numeric_type_changes=True,
             )
-            .assert_json_equal(
+            .assert_json_equal_with_java(
                 "body.json",
                 {
                     "foo": "foo",
                     "bar": [1, 2, 3, 4],
                     "baz": 1.0,
                 },
-                ignore_numeric_type_changes=True,
             )
         ),
     ]
@@ -69,6 +49,8 @@ class TestValidateFailed(HttpRunner):
             .with_json({"foo": "foo", "bar": [1, 2, 3, 4], "baz": 1})
             .validate()
             .assert_equal("status_code", 200)
-            .assert_json_contains("body.json", {"bar": [4, 4, 2, 1], "baz": 1.0})
+            .assert_json_contains_with_java(
+                "body.json", {"bar": [4, 4, 2, 1], "baz": 1.0}
+            )
         ),
     ]
