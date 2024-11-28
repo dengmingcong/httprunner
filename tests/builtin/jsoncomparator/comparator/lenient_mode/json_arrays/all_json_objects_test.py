4bf478b435430da46087cdf2a1e632d3b9605b73
dengmingcong <mc.deng@outlook.com>
2024-11-28 11:16:33 +0800

    test: value contains boolean or 1.0

--- tests/builtin/jsoncomparator/comparator/lenient_mode/json_arrays/all_json_objects_test.py
+++ tests/builtin/jsoncomparator/comparator/lenient_mode/json_arrays/all_json_objects_test.py
@@ -9,3 +9,17 @@ class TestAllJSONObjects:
             [{"a": 1}, {"a": 2}, {"a": 3}], [{"a": 3}, {"a": 2}, {"a": 1}]
         )
         assert result.is_success
+
+    def test_value_contains_boolean_and_equal(self):
+        result = self.json_comparator.compare_json(
+            [{"a": True}, {"a": False}, {"a": 1}, {"a": 0}],
+            [{"a": 1}, {"a": 0}, {"a": False}, {"a": True}],
+        )
+        assert result.is_success
+
+    def test_1p0_equal_1(self):
+        result = self.json_comparator.compare_json(
+            [{"a": 1.0}, {"a": 2.0}, {"a": 3.0}],
+            [{"a": 1}, {"a": 2.0}, {"a": 3}],
+        )
+        assert result.is_success
