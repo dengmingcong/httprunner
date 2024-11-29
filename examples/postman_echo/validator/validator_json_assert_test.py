3a9729ff09d03cdc8b24aedfc7b87dc66c74862d
dengmingcong <mc.deng@outlook.com>
2024-11-29 14:05:25 +0800

    test: error message format

--- examples/postman_echo/validator/validator_json_assert_test.py
+++ examples/postman_echo/validator/validator_json_assert_test.py
@@ -72,3 +72,58 @@ class TestValidateFailed(HttpRunner):
             .assert_json_contains("body.json", {"bar": [4, 4, 2, 1], "baz": 1.0})
         ),
     ]
+
+
+@pytest.mark.xfail(raises=ValidationFailure)
+class TestFailMessage(HttpRunner):
+    config = (
+        Config("test error message format")
+        .base_url("https://postman-echo.com")
+        .verify(False)
+        .continue_on_failure()
+    )
+
+    teststeps = [
+        Step(
+            RunRequest("test error message format")
+            .post("/post")
+            .with_json(
+                {
+                    "type_changes": 1.1,
+                    "values_changed": "new",
+                    "item_added": "this item is unexpected",
+                    "list_not_expected": [1, 2, 3, 3],
+                }
+            )
+            .validate()
+            .assert_equal("status_code", 200)
+            .assert_json_equal(
+                "body.json",
+                {
+                    "type_changes": 1,
+                    "values_changed": "old",
+                    "item_removed": "this item is missing",
+                    "list_not_expected": [1, 2, 3, 4],
+                },
+                ignore_string_type_changes=True,
+            )
+        ),
+        Step(
+            RunRequest("list not expected")
+            .post("/post")
+            .with_json(
+                {
+                    "list_not_expected": [1, 2, 3, 3],
+                }
+            )
+            .validate()
+            .assert_equal("status_code", 200)
+            .assert_json_contains(
+                "body.json",
+                {
+                    "list_not_expected": [1, 2, 3, 4],
+                },
+                ignore_string_type_changes=True,
+            )
+        ),
+    ]
