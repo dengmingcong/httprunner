b9bf1846bea49f53a0243498f4ed534bdd3e4788
dengmingcong <mc.deng@outlook.com>
2024-11-29 15:16:47 +0800

    refactor: remove raise statement

--- tests/builtin/jsoncomparator/jsonassert_test.py
+++ tests/builtin/jsoncomparator/jsonassert_test.py
@@ -1,8 +1,5 @@
-import pytest
-
 from httprunner.builtin.jsoncomparator.jsonassert import json_contains_v2
 
 
 def test_string():
-    with pytest.raises(AssertionError):
-        json_contains_v2("Joe", "Joe")
+    json_contains_v2("Joe", "Joe")
